0ae8119a9441bfd4a42c02134a6e0966c16c68cc
Vicious <maciej.vicious@gmail.com>
2016-05-06 00:30:30 +0200

    Assetic & bootstrap fixes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+	    new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+	    new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev'], true)) {
